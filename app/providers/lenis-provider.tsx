3c46aa1e72fab340cdba6388df19461147f95900
Pedro Osnaghi <osnaghi.prog@gmail.com>
2025-01-31 03:58:34 -0300

    error build corrections lenis

--- app/providers/lenis-provider.tsx
+++ app/providers/lenis-provider.tsx
@@ -1,18 +1,13 @@
 "use client";
-import { ReactLenis } from "@studio-freight/react-lenis";
-import React, { useRef } from "react";
+import { ReactLenis } from "lenis/react";
+import React, { ReactNode } from "react";
 
 interface LenisScrollProviderProps {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 const LenisScrollProvider = ({ children }: LenisScrollProviderProps) => {
-  const lenisRef = useRef(null);
   return (
-    <ReactLenis
-      ref={lenisRef}
-      root
-      options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}
-    >
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
       {children}
     </ReactLenis>
   );
